f982a3a93155cd2a4194eaa40e820cc3bcb819c5
Mims H. Wright <mims@mimswright.com>
2017-01-08 22:05:19 +0100

    Moved tests from mocha to tape

--- test/matrixUtil.test.js
+++ test/matrixUtil.test.js
@@ -1,28 +1,40 @@
-import chai from 'chai'
-const expect = chai.expect
+import test from 'tape'
 import * as matrixUtil from '../src/matrixUtil.js'
 
-describe('matrixUtil', () => {
-  describe('createEmptyArray()', () => {
+test('matrixUtil', (t) => {
+  test('createEmptyArray()', (assert) => {
     let emptyArray = matrixUtil.createEmptyArray(10)
-    expect(emptyArray).to.have.lengthOf(10)
-    expect(emptyArray.every(element => element === 0)).to.equal(true)
+    assert.equal(emptyArray.length, 10, 'Contains n elements, where n is the first parameter')
+    assert.ok(emptyArray.every(element => element === 0), 'All elements should be 0')
+
+    emptyArray = matrixUtil.createEmptyArray(0)
+    assert.equal(emptyArray.length, 0, 'Creating totally empty (length=0) arrays is ok.')
+
+    assert.end()
   })
 
-  describe('createEmptyMatrix()', () => {
+  test('createEmptyMatrix()', (assert) => {
     let emptyMatrix = matrixUtil.createEmptyMatrix(4, 6)
-    expect(emptyMatrix).to.have.lengthOf(6)
-    expect(emptyMatrix.every(row => row.length === 4)).to.equal(true)
-    expect(emptyMatrix[0].every(element => element === 0)).to.equal(true)
+    assert.ok(emptyMatrix.every(row => row.length === 4), 'First parameter is the "width" of the Matrix')
+    assert.equal(emptyMatrix.length, 6, 'First parameter is the "height" of the Matrix')
+    assert.ok(emptyMatrix[0].every(element => element === 0), 'Creates rows of 0')
+
+    assert.end()
   })
 
-  describe('getMatrixWidth()', () => {
+  test('getMatrixWidth()', (assert) => {
     let emptyMatrix = matrixUtil.createEmptyMatrix(4, 6)
-    expect(matrixUtil.getMatrixWidth(emptyMatrix)).to.equal(4)
+    assert.equal(matrixUtil.getMatrixWidth(emptyMatrix), 4)
+
+    assert.end()
   })
 
-  describe('getMatrixHeight()', () => {
+  test('getMatrixHeight()', (assert) => {
     let emptyMatrix = matrixUtil.createEmptyMatrix(4, 6)
-    expect(matrixUtil.getMatrixHeight(emptyMatrix)).to.equal(6)
+    assert.equal(matrixUtil.getMatrixHeight(emptyMatrix), 6)
+
+    assert.end()
   })
+
+  t.end()
 })
