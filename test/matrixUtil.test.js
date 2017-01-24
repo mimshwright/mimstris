4c55af91fa1c3cefaf9a747325882b112bb836e3
Mims H. Wright <mims@mimswright.com>
2017-01-24 23:48:54 +0100

    Revamped testing. already using nyc to cover one extra line

--- test/matrixUtil.test.js
+++ test/matrixUtil.test.js
@@ -1,225 +1,195 @@
-import test from 'tape-catch'
+import test from 'ava'
 import * as matrixUtil from '../src/matrixUtil.js'
 
-test('matrixUtil', (t) => {
-  const mockMatrix = [[0, 0, 0], [0, 1, 0], [1, 1, 1]]
+const mockMatrix = [[0, 0, 0], [0, 1, 0], [1, 1, 1]]
 
-  test('createEmptyArray()', (assert) => {
-    let emptyArray = matrixUtil.createEmptyArray(10)
-    assert.equal(emptyArray.length, 10, 'Contains n elements, where n is the first parameter')
-    assert.ok(emptyArray.every(element => element === 0), 'All elements should be 0')
+test('createEmptyArray()', (assert) => {
+  let emptyArray = matrixUtil.createEmptyArray(10)
+  assert.is(emptyArray.length, 10, 'Contains n elements, where n is the first parameter')
+  assert.truthy(emptyArray.every(element => element === 0), 'All elements should be 0')
 
-    emptyArray = matrixUtil.createEmptyArray(0)
-    assert.equal(emptyArray.length, 0, 'Creating totally empty (length=0) arrays is ok.')
+  emptyArray = matrixUtil.createEmptyArray(0)
+  assert.is(emptyArray.length, 0, 'Creating totally empty (length=0) arrays is ok.')
+})
 
-    assert.end()
-  })
+test('createEmptyMatrix()', (assert) => {
+  let emptyMatrix = matrixUtil.createEmptyMatrix(4, 6)
+  assert.truthy(emptyMatrix.every(row => row.length === 4), 'First parameter is the "width" of the Matrix')
+  assert.is(emptyMatrix.length, 6, 'First parameter is the "height" of the Matrix')
+  assert.truthy(emptyMatrix[0].every(element => element === 0), 'Creates rows of 0')
+})
 
-  test('createEmptyMatrix()', (assert) => {
-    let emptyMatrix = matrixUtil.createEmptyMatrix(4, 6)
-    assert.ok(emptyMatrix.every(row => row.length === 4), 'First parameter is the "width" of the Matrix')
-    assert.equal(emptyMatrix.length, 6, 'First parameter is the "height" of the Matrix')
-    assert.ok(emptyMatrix[0].every(element => element === 0), 'Creates rows of 0')
+test('getMatrixWidth()', (assert) => {
+  let emptyMatrix = matrixUtil.createEmptyMatrix(4, 6)
+  assert.is(matrixUtil.getMatrixWidth(emptyMatrix), 4)
+})
 
-    assert.end()
-  })
+test('getMatrixHeight()', (assert) => {
+  let emptyMatrix = matrixUtil.createEmptyMatrix(4, 6)
+  assert.is(matrixUtil.getMatrixHeight(emptyMatrix), 6)
+})
 
-  test('getMatrixWidth()', (assert) => {
-    let emptyMatrix = matrixUtil.createEmptyMatrix(4, 6)
-    assert.equal(matrixUtil.getMatrixWidth(emptyMatrix), 4)
+test('getMatrixSize()', (assert) => {
+  let emptyMatrix = matrixUtil.createEmptyMatrix(4, 6)
+  const {width, height} = matrixUtil.getMatrixSize(emptyMatrix)
+  assert.is(width, 4, 'Returns and object with correct width')
+  assert.is(height, 6, 'Returns and object with correct height')
+})
 
-    assert.end()
-  })
+test('removeRow()', (assert) => {
+  assert.is(matrixUtil.getMatrixHeight(mockMatrix), 3, 'mockMatrix has 3 rows')
+  let testMatrix = matrixUtil.removeRow(mockMatrix, 1)
+  assert.pass('Removing row at index 1 from a 3x3 matrix')
+  assert.is(matrixUtil.getMatrixHeight(testMatrix), 2, 'Removed 1 row')
+  assert.truthy(testMatrix[0].every(val => val === 0), 'Row 0 unchanged')
+  assert.truthy(testMatrix[1].every(el => el === 1), 'Row 2 is now row 1.')
+  assert.is(matrixUtil.getMatrixHeight(mockMatrix), 3, 'mockMatrix is unmodified')
+})
 
-  test('getMatrixHeight()', (assert) => {
-    let emptyMatrix = matrixUtil.createEmptyMatrix(4, 6)
-    assert.equal(matrixUtil.getMatrixHeight(emptyMatrix), 6)
+test('removeColumn()', (assert) => {
+  assert.is(matrixUtil.getMatrixWidth(mockMatrix), 3, 'mockMatrix has 3 columns')
+  let testMatrix = matrixUtil.removeColumn(mockMatrix, 1)
+  assert.pass('Removing column at index 1 from a 3x3 matrix')
+  assert.is(matrixUtil.getMatrixWidth(testMatrix), 2, 'Removed 1 column')
+  assert.truthy(testMatrix[1].every(val => val === 0), 'Removed the single 1 from middle row')
+  assert.is(matrixUtil.getMatrixWidth(mockMatrix), 3, 'mockMatrix is unmodified')
+})
 
-    assert.end()
-  })
+test('removeRowAndShiftRemaining()', (assert) => {
+  assert.is(matrixUtil.getMatrixHeight(mockMatrix), 3, 'mockMatrix has 3 rows')
+  let testMatrix = matrixUtil.removeRowAndShiftRemaining(mockMatrix, 1)
+  assert.pass('Removing row at index 1 from a 3x3 matrix and shifting')
+  assert.is(matrixUtil.getMatrixHeight(testMatrix), 3, 'Removed 1 row but height is same')
+  assert.truthy(testMatrix[0].every(val => val === 0), 'Row 0 is a new empty row of 0s')
+  assert.truthy(testMatrix[1].every(val => val === 0), 'Row 0 unchanged but now in position 1')
+  assert.truthy(testMatrix[2].every(el => el === 1), 'Row 2 is unchanged.')
+  assert.is(matrixUtil.getMatrixHeight(mockMatrix), 3, 'mockMatrix is unmodified')
+})
 
-  test('getMatrixSize()', (assert) => {
-    let emptyMatrix = matrixUtil.createEmptyMatrix(4, 6)
-    const {width, height} = matrixUtil.getMatrixSize(emptyMatrix)
-    assert.equal(width, 4, 'Returns and object with correct width')
-    assert.equal(height, 6, 'Returns and object with correct height')
+test('combineMatrices()', (assert) => {
+  let a = [
+    [1, 1, 0],
+    [1, 1, 0],
+    [0, 0, 0]
+  ]
+  let b = [
+    [2, 2],
+    [2, 2]
+  ]
 
-    assert.end()
-  })
+  let combined = matrixUtil.combineMatrices(a, b, 1, 1)
+  let expected = [
+    [1, 1, 0],
+    [1, 2, 2],
+    [0, 2, 2]
+  ]
+  assert.deepEqual(combined, expected, 'Combines correctly.')
 
-  test('removeRow()', (assert) => {
-    assert.equal(matrixUtil.getMatrixHeight(mockMatrix), 3, 'mockMatrix has 3 rows')
-    let testMatrix = matrixUtil.removeRow(mockMatrix, 1)
-    assert.comment('Removing row at index 1 from a 3x3 matrix')
-    assert.equal(matrixUtil.getMatrixHeight(testMatrix), 2, 'Removed 1 row')
-    assert.ok(testMatrix[0].every(val => val === 0), 'Row 0 unchanged')
-    assert.ok(testMatrix[1].every(el => el === 1), 'Row 2 is now row 1.')
-    assert.equal(matrixUtil.getMatrixHeight(mockMatrix), 3, 'mockMatrix is unmodified')
+  combined = matrixUtil.combineMatrices(a, b, 1, 1, false)
+  expected = [
+    [1, 1, 0],
+    [1, 1, 2],
+    [0, 2, 2]
+  ]
+  assert.deepEqual(combined, expected, "Combines correctly. If 5th param is false, doesn't overwrite non-zero values")
 
-    assert.end()
-  })
+  assert.throws(assert => { matrixUtil.combineMatrices(a, [[]], 0, 0) }, Error, "Bogus matrices throw errors.")
+})
 
-  test('removeColumn()', (assert) => {
-    assert.equal(matrixUtil.getMatrixWidth(mockMatrix), 3, 'mockMatrix has 3 columns')
-    let testMatrix = matrixUtil.removeColumn(mockMatrix, 1)
-    assert.comment('Removing column at index 1 from a 3x3 matrix')
-    assert.equal(matrixUtil.getMatrixWidth(testMatrix), 2, 'Removed 1 column')
-    assert.ok(testMatrix[1].every(val => val === 0), 'Removed the single 1 from middle row')
-    assert.equal(matrixUtil.getMatrixWidth(mockMatrix), 3, 'mockMatrix is unmodified')
+test('detectCollision', (assert) => {
+  let a = [
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 2, 0, 0, 0, 0],
+    [2, 0, 2, 2, 0, 0, 0, 0]
+  ]
+  let b = [
+    [1, 1, 1, 0],
+    [0, 1, 0, 0],
+    [0, 0, 0, 0]
+  ]
+  const f = matrixUtil.detectCollision
 
-    assert.end()
-  })
+  assert.truthy(f(a, b) === false, "Doesn't detect collision with values set to 0. By default, offsets are 0")
+  assert.truthy(f(a, b, 1, 1), 'Detect collision with values set to non-zero.')
+  assert.truthy(f(a, b, -1, 0), 'Going outside of x boundaries to the left is considered collision.')
+  assert.truthy(f(a, b, 6, 0), 'Going outside of x boundaries to the right is considered collision.')
+  assert.truthy(f(a, b, 4, 3), 'Going outside of y boundaries is considered collision.')
+  assert.truthy(f(a, b, 4, 0) === false, 'Going outside of x boundaries with only 0 values is NOT considered collision.')
+  assert.truthy(f(a, b, 5, 0) === false, 'Going outside of x boundaries with only 0 values is NOT considered collision.')
+  assert.truthy(f(a, b, 4, 1) === false, 'Going outside of y boundaries with only 0 values is NOT considered collision.')
+  assert.truthy(f(a, b, 1, 0) === false, 'Collision with only 0 values is NOT considered collision.')
 
-  test('removeRowAndShiftRemaining()', (assert) => {
-    assert.equal(matrixUtil.getMatrixHeight(mockMatrix), 3, 'mockMatrix has 3 rows')
-    let testMatrix = matrixUtil.removeRowAndShiftRemaining(mockMatrix, 1)
-    assert.comment('Removing row at index 1 from a 3x3 matrix and shifting')
-    assert.equal(matrixUtil.getMatrixHeight(testMatrix), 3, 'Removed 1 row but height is same')
-    assert.ok(testMatrix[0].every(val => val === 0), 'Row 0 is a new empty row of 0s')
-    assert.ok(testMatrix[1].every(val => val === 0), 'Row 0 unchanged but now in position 1')
-    assert.ok(testMatrix[2].every(el => el === 1), 'Row 2 is unchanged.')
-    assert.equal(matrixUtil.getMatrixHeight(mockMatrix), 3, 'mockMatrix is unmodified')
+  a = [
+    [0, 0, 0, 0],
+    [0, 0, 0, 0],
+    [0, 2, 2, 0],
+    [2, 2, 0, 0]
+  ]
+  b = [
+    [0, 1, 0],
+    [0, 1, 0],
+    [1, 1, 0]
+  ]
+  assert.truthy(f(a, b, 2, 0), 'Do detect collision when pieces overlap.')
+  assert.truthy(f(a, b, 2, 1) === false, 'Don\'t detect collision when pieces don\'t overlap.')
+})
 
-    assert.end()
-  })
+test('flip()', (assert) => {
+  let flipped = matrixUtil.flip([
+    [0, 0, 0],
+    [0, 1, 0],
+    [1, 1, 0]
+  ])
+  let expected = [
+    [0, 0, 1],
+    [0, 1, 1],
+    [0, 0, 0]
+  ]
+  let msg = 'Values flipped as expected'
+  assert.deepEqual(flipped, expected, msg)
+})
 
-  test('combineMatrices()', (assert) => {
-    let a = [
-      [1, 1, 0],
-      [1, 1, 0],
-      [0, 0, 0]
-    ]
-    let b = [
-      [2, 2],
-      [2, 2]
-    ]
+test('mirror()', (assert) => {
+  let mirrored = matrixUtil.mirror([
+    [1, 0, 0],
+    [0, 1, 1],
+    [1, 1, 0]
+  ])
+  let expected = [
+    [0, 0, 1],
+    [1, 1, 0],
+    [0, 1, 1]
+  ]
+  let msg = 'Values mirrored as expected'
+  assert.deepEqual(mirrored, expected, msg)
+})
 
-    let combined = matrixUtil.combineMatrices(a, b, 1, 1)
-    let expected = [
-      [1, 1, 0],
-      [1, 2, 2],
-      [0, 2, 2]
-    ]
-    assert.deepEqual(combined, expected, 'Combines correctly.')
+test('rotateLeft()', (assert) => {
+  let rotated = matrixUtil.rotateLeft([
+    [0, 0, 0],
+    [0, 1, 0],
+    [1, 1, 0]
+  ])
+  let expected = [
+    [0, 0, 0],
+    [0, 1, 1],
+    [0, 0, 1]
+  ]
+  let msg = 'Values rotated as expected'
+  assert.deepEqual(rotated, expected, msg)
+})
 
-    combined = matrixUtil.combineMatrices(a, b, 1, 1, false)
-    expected = [
-      [1, 1, 0],
-      [1, 1, 2],
-      [0, 2, 2]
-    ]
-    assert.deepEqual(combined, expected, "Combines correctly. If 5th param is false, doesn't overwrite non-zero values")
-
-    assert.end()
-  })
-
-  test('detectCollision', (assert) => {
-    let a = [
-      [0, 0, 0, 0, 0, 0, 0, 0],
-      [0, 0, 0, 2, 0, 0, 0, 0],
-      [2, 0, 2, 2, 0, 0, 0, 0]
-    ]
-    let b = [
-      [1, 1, 1, 0],
-      [0, 1, 0, 0],
-      [0, 0, 0, 0]
-    ]
-    const f = matrixUtil.detectCollision
-
-    assert.ok(f(a, b) === false, "Doesn't detect collision with values set to 0. By default, offsets are 0")
-    assert.ok(f(a, b, 1, 1), 'Detect collision with values set to non-zero.')
-    assert.ok(f(a, b, -1, 0), 'Going outside of x boundaries to the left is considered collision.')
-    assert.ok(f(a, b, 6, 0), 'Going outside of x boundaries to the right is considered collision.')
-    assert.ok(f(a, b, 4, 3), 'Going outside of y boundaries is considered collision.')
-    assert.ok(f(a, b, 4, 0) === false, 'Going outside of x boundaries with only 0 values is NOT considered collision.')
-    assert.ok(f(a, b, 5, 0) === false, 'Going outside of x boundaries with only 0 values is NOT considered collision.')
-    assert.ok(f(a, b, 4, 1) === false, 'Going outside of y boundaries with only 0 values is NOT considered collision.')
-    assert.ok(f(a, b, 1, 0) === false, 'Collision with only 0 values is NOT considered collision.')
-
-    a = [
-      [0, 0, 0, 0],
-      [0, 0, 0, 0],
-      [0, 2, 2, 0],
-      [2, 2, 0, 0]
-    ]
-    b = [
-      [0, 1, 0],
-      [0, 1, 0],
-      [1, 1, 0]
-    ]
-    assert.ok(f(a, b, 2, 0), 'Do detect collision when pieces overlap.')
-    assert.ok(f(a, b, 2, 1) === false, 'Don\'t detect collision when pieces don\'t overlap.')
-
-    assert.end()
-  })
-
-  test('flip()', (assert) => {
-    let flipped = matrixUtil.flip([
-      [0, 0, 0],
-      [0, 1, 0],
-      [1, 1, 0]
-    ])
-    let expected = [
-      [0, 0, 1],
-      [0, 1, 1],
-      [0, 0, 0]
-    ]
-    let msg = 'Values flipped as expected'
-    assert.deepEqual(flipped, expected, msg)
-
-    assert.end()
-  })
-
-  test('mirror()', (assert) => {
-    let mirrored = matrixUtil.mirror([
-      [1, 0, 0],
-      [0, 1, 1],
-      [1, 1, 0]
-    ])
-    let expected = [
-      [0, 0, 1],
-      [1, 1, 0],
-      [0, 1, 1]
-    ]
-    let msg = 'Values mirrored as expected'
-    assert.deepEqual(mirrored, expected, msg)
-
-    assert.end()
-  })
-
-  test('rotateLeft()', (assert) => {
-    let rotated = matrixUtil.rotateLeft([
-      [0, 0, 0],
-      [0, 1, 0],
-      [1, 1, 0]
-    ])
-    let expected = [
-      [0, 0, 0],
-      [0, 1, 1],
-      [0, 0, 1]
-    ]
-    let msg = 'Values rotated as expected'
-    assert.deepEqual(rotated, expected, msg)
-
-    assert.end()
-  })
-
-  test('rotateRight()', (assert) => {
-    let rotated = matrixUtil.rotateRight([
-      [0, 0, 0],
-      [0, 1, 0],
-      [1, 1, 0]
-    ])
-    let expected = [
-      [1, 0, 0],
-      [1, 1, 0],
-      [0, 0, 0]
-    ]
-    let msg = 'Values rotated as expected'
-    assert.deepEqual(rotated, expected, msg)
-
-    assert.end()
-  })
-
-  t.end()
+test('rotateRight()', (assert) => {
+  let rotated = matrixUtil.rotateRight([
+    [0, 0, 0],
+    [0, 1, 0],
+    [1, 1, 0]
+  ])
+  let expected = [
+    [1, 0, 0],
+    [1, 1, 0],
+    [0, 0, 0]
+  ]
+  let msg = 'Values rotated as expected'
+  assert.deepEqual(rotated, expected, msg)
 })
