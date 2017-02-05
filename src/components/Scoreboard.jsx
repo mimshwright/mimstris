1ab243f763f531b7c871610112c72aa6d5a29437
Mims H. Wright <mims@mimswright.com>
2017-02-05 10:37:27 +0100

    Added commas to the score

--- src/components/Scoreboard.jsx
+++ src/components/Scoreboard.jsx
@@ -1,9 +1,10 @@
 import React, {PropTypes} from 'react'
+import commaNumber from 'comma-number'
 
 const Scoreboard = props => (
   <div className='scoreboard'>
     <div className='scoreboardText' id='level'>Level: {props.level}</div>
-    <div className='scoreboardText' id='score'>Score: {props.score}</div>
+    <div className='scoreboardText' id='score'>Score: {commaNumber(props.score)}</div>
     <div className='scoreboardText' id='lines'>Lines: {props.lines}</div>
   </div>
 )
