3e5e946ad35ae3385fe5a3a925378706cb2e015e
Mims H. Wright <mims@mimswright.com>
2017-03-16 16:32:58 +0100

    Updated instructions

--- src/components/Instructions.jsx
+++ src/components/Instructions.jsx
@@ -2,6 +2,6 @@ import React from 'react'
 
 export default props => (
   <p className='instructions'>
-    WASD or Arrow keys to move - Shift + Z or Shift and / to Rotate - Enter to pause or restart
+    WASD or Arrow keys to move - Shift, Z, / or up to Rotate - Enter to pause or restart
   </p>
 )
